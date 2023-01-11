1d895250907dbaef0d77cc8576bfae9ca24171ff
Herbert Cabus Jacby <111984596+HerbertCJ@users.noreply.github.com>
2023-01-11 09:47:11 -0300

    added JS files and hooks

--- src/App.js
+++ src/App.js
@@ -1,4 +1,12 @@
+import { useState } from "react";
+
 function App() {
+  const [name, setName] = useState('')
+  const [list, setList] = useState([])
+  const [editId, setEditId] = useState(null)
+  const [isEditing, setIsEditing] = useState(false)
+  const [alert, setAlert] = useState({status: false, type: '', msg: ''})
+
   return (
     <div>
       <h1>Hello World!</h1>
